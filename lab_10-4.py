88e4b5c65fd9aa44a10a4fe2edae95acb830a33a
Jan Salafia <p23jsalafia@fairfieldprep.org>
2023-01-27 12:30:02 -0500

    Lab 10-4, 10-5

--- lab_10-4.py
+++ lab_10-4.py
@@ -1,6 +1,10 @@
 #Jan Salafia Lab 10-4 1/23/2023
+names = "John", "Jane", "Bob"
+def indexed_names(names):
+    indexed_list = []
+    for index,name in enumerate(names):
+        indexed_name = str(index) + ": " + name
+        indexed_list.append(indexed_name)
+    return
 
-def indexed_names(names)
-    for index,value in enumerate(names)
-        broken_names = [*value]                        #BReak apart names
-        names= "".join(broken_names)                   #Rejoin names
+print(indexed_names(names))
--- lab_10-5.py
+++ lab_10-5.py
@@ -8,16 +8,13 @@ def add_foods(list):
             TEindex = index
             letter_list = [*value]
             sixth_letter.append(letter_list[5])
+            print("The sixth letter is " + letter_list)
     except TypeError:
         print("Invalid Input")
     except IndexError:
         print(value + " is a short food")
-
-    sixth_letter_list = print("The sixth letter is " + sixth_letter)
-    return(sixth_letter)
+    return
 
 add_foods(["potatoes", "salsa", "cherries", "banana", "apple"])
 add_foods(["naan", "celery", "buckwheat", 7, "clementine"])
 add_foods(["cheeseburger", True, "chicken", "rice", "radish"])
-
-sixth_letter_list = print("The sixth letter is " , sixth_letter)
